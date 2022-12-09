5791c8bfd123bec5f92223b88cfc566145996f74
Izumi 和泉 <112969240+ikx7a@users.noreply.github.com>
2022-12-09 10:12:08 +0530

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,6 @@
+echo "Cloning Repo...."
+git clone https://github.com/Al3x-GitHub/ShikimoriSanBot /ShikimoriSanBot
+cd /ShikimoriSanBot
+pip3 install -r requirements.txt
+echo "Starting Bot...."
+python3 bot.py
